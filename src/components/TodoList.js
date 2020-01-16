c0d84e98956eaf6b747c4a358d8b142b37a4f0ac
hemagunti <hema.gunti@gmail.com>
2020-01-16 23:11:15 +0100

    created todo-list app

---
+++ src/components/TodoList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+export default class TodoList extends Component {
+    render() {
+        return (
+            <ul className="list-group my-5">
+                <h3 className="text-capitalize text-center">Todo List</h3>
+                <TodoItem />
+                <button type="button" className="btn btn-danger btn-block text-capitalize my-5">
+                    clear list
+                </button>
+            </ul>
+
+
+
+        )
+    }
+}
